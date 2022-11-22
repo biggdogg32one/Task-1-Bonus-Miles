45cae8a2bac6054554a8234fea9b36ac7e830678
GIT Test Sergey <biggdogg97@gmail.com>
2022-11-22 16:56:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 100_000;
+        int miles = service.calculate(price);
+
+        System.out.println(miles);
+    }
+}
